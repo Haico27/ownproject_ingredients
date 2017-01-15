3da0e3cc0028a21c6ae83941e6c187dc09e85b1b
Haico27 <haico.wensink@gmail.com>
2017-01-15 21:11:24 +0100

    Adding model relationships and foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115200520) do
+ActiveRecord::Schema.define(version: 20170115200952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170115200520) do
     t.string   "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "meal_id"
+    t.index ["meal_id"], name: "index_ingredients_on_meal_id", using: :btree
   end
 
   create_table "meals", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 20170115200520) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ingredients", "meals"
 end
