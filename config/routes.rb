7e459827a15472612d826c15db11f99db0b44da2
Haico27 <haico.wensink@gmail.com>
2017-01-15 21:53:25 +0100

    Adding route, controller and view Ingredient

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  root "ingredients#index"
+
+  get "ingredients" => "ingredients#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
