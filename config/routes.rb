ee6ab1db181fa7bc1682bbcc1c55dd08fd5e8da7
Haico27 <haico.wensink@gmail.com>
2017-01-15 22:11:49 +0100

    Create show ingredient page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "ingredients#index"
 
   get "ingredients" => "ingredients#index"
+  get "ingredients/:id" => "ingredients#show", as: :ingredient
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
